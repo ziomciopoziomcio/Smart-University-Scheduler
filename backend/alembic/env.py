cb4d5846a1fd3d0bd48bf2bda36d33f475df4f6a
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-05 19:53:29 +0100

    Reformat

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,13 +1,11 @@
+import os
 from logging.config import fileConfig
 
+from alembic import context
+# Added to download environment variables from .env file
+from dotenv import load_dotenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
-from alembic import context
-
-# Added to download environment variables from .env file
-import os
-from dotenv import load_dotenv
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
