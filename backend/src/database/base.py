6b1103ed35e0e37685cb64a8399465300d143e45
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-05 20:10:56 +0100

    Added DeclarativeBase into base.py

--- backend/src/database/base.py
+++ backend/src/database/base.py
@@ -1,3 +1,12 @@
 """
 Class Base as the base for all models in the database.
 """
+
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    """
+    Base class for all models in the database.
+    """
+    pass
