a5956cb84948a77f2eaf40d7dcb89330106a3da9
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-05 20:49:08 +0100

    Added user_roles association table

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -10,6 +10,15 @@ from sqlalchemy import String, Text, DateTime, text, Table, Column, Integer, For
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database.base import Base
 
+user_roles = Table(
+    'user_roles',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('role_id', Integer, ForeignKey('roles.id'), primary_key=True),
+
+    UniqueConstraint('user_id', 'role_id', name='uq_user_role')
+)
+
 
 class User(Base):
     """User model representing a user in the system."""
