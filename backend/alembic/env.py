0621b74b28b9ffd6727fbacfebc3d689b30f472c
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-07 23:05:36 +0100

    syntax fix

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -94,7 +94,7 @@ def run_migrations_online() -> None:
     with connectable.connect() as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata,
-            compare_type=True, compare_server_default=True
+            compare_type=True, compare_server_default=True,
         )
 
         with context.begin_transaction():
