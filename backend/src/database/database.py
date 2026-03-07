576cc7882231836c485358c05c9344e1fbebb2cd
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-07 03:05:07 +0100

    Corrected .env naming

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -10,11 +10,11 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("DB_USER")
+POSTGRES_PASSWORD = os.getenv("DB_PASSWORD")
+POSTGRES_HOST = os.getenv("DB_HOST")
+POSTGRES_PORT = os.getenv("DB_PORT")
+POSTGRES_DB = os.getenv("DB_NAME")
 
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
