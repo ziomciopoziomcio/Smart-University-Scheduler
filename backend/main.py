e441026b4f84fd770007795e80cbbccdfc8519be
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-18 16:37:40 +0100

    Added lifespan to main

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,45 @@
+import asyncio
 import os
+from contextlib import asynccontextmanager
 
+from aiokafka import AIOKafkaProducer
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src import api_routers
+from src.common.kafka_client import kafka_manager
 
 load_dotenv()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    max_retries = 5
+    for attempt in range(max_retries):
+        try:
+            kafka_manager.producer = AIOKafkaProducer(
+                bootstrap_servers=os.getenv("KAFKA_URL", "localhost:9092"),
+            )
+            await kafka_manager.producer.start()
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+            await asyncio.sleep(5)
+    yield
+
+    try:
+        if kafka_manager.producer:
+            await kafka_manager.producer.stop()
+    except Exception as e:
+        print(f"Error during Kafka producer shutdown: {e}")
+
 
 app = FastAPI(
     title="Smart University Scheduler API",
     description="API for managing SUS system",
     version="v.0.0.1-alpha",
+    lifespan=lifespan,
 )
 
 origins = [
