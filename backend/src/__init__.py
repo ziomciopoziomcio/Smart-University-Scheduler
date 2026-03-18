f3a42c81b8450b85dfe5a634368329b736337a2d
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-18 16:37:23 +0100

    Added schedules_router to api_routers

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -2,10 +2,12 @@ from .academics.router import router as academics_router
 from .courses.router import router as courses_router
 from .facilities.router import router as facilities_router
 from .users.router import router as users_router
+from .schedules.router import router as schedules_router
 
 api_routers = [
     academics_router,
     courses_router,
     facilities_router,
     users_router,
+    schedules_router,
 ]
