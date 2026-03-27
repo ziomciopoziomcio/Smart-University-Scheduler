5d509b72b75e892e326635cb2e8eb3e5f4b923d1
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-27 13:53:36 +0100

    Added _check_collisions

--- ai_worker/optimizer/fitness.py
+++ ai_worker/optimizer/fitness.py
@@ -15,6 +15,8 @@ class FitnessCalculator:
         self.W_FATIGUE = 150  # day longer than 6 hours
         self.W_TOO_MUCH_STUDENTS = 5000
 
+        self.W_HARD_PENALTY = 100000
+
     def calculate_fitness(self, chromosome: ScheduleChromosome) -> float:
         """Calculates fitness score for a given schedule chromosome
         :param chromosome: ScheduleChromosome to calculate fitness for
@@ -46,4 +48,27 @@ class FitnessCalculator:
                     wasted = room_cap - gene.group_size
                     penalty += self.W_GAP_SLOT * wasted
 
+        return penalty
+
+    def _check_collisions(self, chromosome: ScheduleChromosome) -> float:
+        """
+        Helper function to check collisions
+        :param chromosome: ScheduleChromosome to calculate fitness for
+        :return: Penalty score (lower is better)
+        """
+        penalty = 0.0
+        genes = chromosome.genes
+
+        for i in range(len(genes)):
+            for j in range(i + 1, len(genes)):
+                g1, g2 = genes[i], genes[j]
+                if g1.timeslot_id == g2.timeslot_id:
+                    shared_weeks = set(g1.active_weeks) & set(g2.active_weeks)
+                    if shared_weeks:
+                        if (
+                            g1.room_id == g2.room_id
+                            or g1.instructor_id == g2.instructor_id
+                            or g1.group_id == g2.group_id
+                        ):
+                            penalty += self.W_HARD_PENALTY
         return penalty
