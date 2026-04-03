831f64ea553f7ca9c225281f8fad46fdad304f2e
nina <ninazugaj25@gmail.com>
2026-04-03 18:46:38 +0200

    Add imports

---
+++ ai_worker/optimizer/greedy.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from typing import List, Dict, Set, Tuple, Optional
+import random
+
+from .models import ClassSessionGene
+from .fitness import SLOTS_PER_DAY, MAX_SLOT_ID
