9571e57c381c832f0eba3e5922749f06ab1bb62a
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-04-03 16:53:43 +0200

    Added crossover

--- ai_worker/optimizer/evolution.py
+++ ai_worker/optimizer/evolution.py
@@ -1,3 +1,4 @@
+import copy
 import random
 from typing import List
 
@@ -24,3 +25,31 @@ class EvolutionEngine:
             tournament_pool,
             key=lambda chrom: getattr(chrom, "fitness_score", float("inf")),
         )
+
+    @staticmethod
+    def crossover(
+        parents: List[models.ScheduleChromosome],
+    ) -> List[models.ScheduleChromosome]:
+        """
+        Crossover between parents to create offspring
+        :param parents: List of ScheduleChromosome to crossover
+        :return: List of offspring ScheduleChromosome
+        """
+        offspring = []
+
+        for i in range(0, len(parents), 2):
+            parent1 = parents[i]
+            parent2 = parents[i + 1] if i + 1 < len(parents) else parents[0]
+
+            crossover_point = random.randint(1, len(parent1.genes) - 1)
+            child1_genes = copy.deepcopy(
+                parent1.genes[:crossover_point]
+            ) + copy.deepcopy(parent2.genes[crossover_point:])
+            child2_genes = copy.deepcopy(
+                parent2.genes[:crossover_point]
+            ) + copy.deepcopy(parent1.genes[crossover_point:])
+
+            offspring.append(models.ScheduleChromosome(genes=child1_genes))
+            offspring.append(models.ScheduleChromosome(genes=child2_genes))
+
+        return offspring[: len(parents)]
