3b3e877262e57d1c2bfd22ce051d78d04fed7611
Karolina <kbanaszewskaa@gmail.com>
2026-04-08 09:21:51 +0200

    Authorization pages - not finished
    * First page look + configuration
    
    * Fixed design
    
    * Fixed login page
    
    * Added register page
    
    * Activation page
    
    * Added forgot password and reset password page
    
    * Fixed reset page
    
    * Added pl.json
    
    * Delete print
    
    Co-authored-by: Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>
    
    * Changed alt name
    
    Co-authored-by: Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>
    
    * Transalted into english
    
    Co-authored-by: Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>
    
    * Transalted into english
    
    Co-authored-by: Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>
    
    * Clean up commented code in ResetPasswordPage
    
    Removed commented code for success status and error message display.
    
    * Change text to 'MY PLAN PAGE' and update logout button
    
    Updated text in MyPlan component to reflect new page title and content.
    
    * Add TODO for fixing base URL with environment variables
    
    Added a TODO comment to address environment variables for the base URL.
    
    * Update text in Logo.svg to English
    
    ---------
    
    Co-authored-by: Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,122 @@
+import type {AuthResponse} from './types';
+
+//TODO: ENV VARIABLES - FIX URL
+const BASE_URL = 'http://localhost:3000/users';
+
+export const loginUser = async (email: string, password: string): Promise<AuthResponse> => {
+    const formData = new URLSearchParams();
+    formData.append('username', email);
+    formData.append('password', password);
+    const response = await fetch(`${BASE_URL}/login`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: formData,
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.detail || 'Could not login');
+    }
+
+    return response.json() as Promise<AuthResponse>;
+};
+
+export const registerUser = async (userData: any): Promise<void> => {
+    const response = await fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: userData.email,
+            password: userData.password,
+            password2: userData.confirmPassword,
+            name: userData.name,
+            surname: userData.surname,
+            phone_number: userData.phone_number,
+            degree: userData.degree
+        }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        const errorMessage = Array.isArray(errorData.detail)
+            ? errorData.detail[0].msg
+            : errorData.detail;
+        throw new Error(errorMessage || 'Registration failed');
+    }
+
+    return;
+};
+
+export const verify2FA = async (code: string, preAuthToken: string): Promise<AuthResponse> => {
+    const response = await fetch(`${BASE_URL}/2fa/verify`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            code,
+            pre_auth_token: preAuthToken
+        }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.detail || '2FA failed');
+    }
+
+    return response.json() as Promise<AuthResponse>;
+};
+
+export const verifyEmail = async (token: string): Promise<void> => {
+    const response = await fetch(`${BASE_URL}/verify-email?token=${encodeURIComponent(token)}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.detail || 'Activation failed');
+    }
+
+    return;
+};
+
+export const forgotPassword = async (email: string): Promise<void> => {
+    const response = await fetch(`${BASE_URL}/password/forgot`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.detail || 'Forgot password request failed');
+    }
+
+    return;
+};
+
+export const resetPassword = async (payload: any): Promise<void> => {
+    const response = await fetch(`${BASE_URL}/password/reset`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            token: payload.token,
+            password: payload.password,
+            password2: payload.password2,
+        }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.detail || 'Resetting password failed');
+    }
+
+    return;
+};
