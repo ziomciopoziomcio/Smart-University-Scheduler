fe1022f383036f4be8724ef80cd2d96364a2bfb3
Karolina <kbanaszewskaa@gmail.com>
2026-04-08 09:21:51 +0200

    Authorization pages - not finished
    * First page look + configuration
    
    * Fixed design
    
    * Fixed login page
    
    * Added register page
    
    * Activation page
    
    * Added forgot password and reset password page
    
    * Fixed reset page
    
    * Added pl.json
    
    * Delete print
    
    Co-authored-by: Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>
    
    * Changed alt name
    
    Co-authored-by: Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>
    
    * Transalted into english
    
    Co-authored-by: Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>
    
    * Transalted into english
    
    Co-authored-by: Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>
    
    * Clean up commented code in ResetPasswordPage
    
    Removed commented code for success status and error message display.
    
    * Change text to 'MY PLAN PAGE' and update logout button
    
    Updated text in MyPlan component to reflect new page title and content.
    
    * Add TODO for fixing base URL with environment variables
    
    Added a TODO comment to address environment variables for the base URL.
    
    * Update text in Logo.svg to English
    
    ---------
    
    Co-authored-by: Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>

---
+++ frontend/src/AppRoute.tsx
@@ -0,0 +1,36 @@
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
+import LoginPage from './pages/Auth/LoginPage';
+import RegisterPage from './pages/Auth/RegisterPage';
+import ActivationPage from './pages/Auth/ActivationPage.tsx'
+import ForgotPasswordPage from './pages/Auth/ForgotPasswordPage.tsx'
+import ResetPasswordPage from './pages/Auth/ResetPasswordPage.tsx'
+import MyPlan from './pages/Plan/MyPlan.tsx';
+import ProtectedRoute from './components/Login/ProtectedRoute';
+
+function AppRoute() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/activate" element={<ActivationPage/>}/>
+                <Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
+                <Route path="/reset-password" element={<ResetPasswordPage/>}/>
+                <Route element={<ProtectedRoute/>}>
+                    <Route path="/plan" element={<MyPlan/>}/>
+                </Route>
+                <Route
+                    path="/"
+                    element={
+                        localStorage.getItem('token')
+                            ? <Navigate to="/plan" replace/>
+                            : <Navigate to="/login" replace/>
+                    }
+                />
+                <Route path="*" element={<Navigate to="/" replace/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRoute;
