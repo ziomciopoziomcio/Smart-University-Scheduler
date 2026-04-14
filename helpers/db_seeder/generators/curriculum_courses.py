abec97b06a05d73000d21ff38701ec670501a9ef
Wojciech Korbel <251169@edu.p.lodz.pl>
2026-04-15 00:19:06 +0200

    Added _get_unique_study_fields func

--- helpers/db_seeder/generators/curriculum_courses.py
+++ helpers/db_seeder/generators/curriculum_courses.py
@@ -33,9 +33,44 @@ def _get_study_field_major_degree_from_file(
     return sorted(list(unique_combinations))
 
 
+def _get_unique_study_fields(
+    sourcefile: str, study_field_name: str, degree: int
+) -> list:
+    """
+    Load study field data from a JSON file and return unique combinations
+    :param sourcefile: path to JSON file containing study field data
+    :param study_field_name: name of the study field to filter
+    :param degree: degree to filter
+    :return: a list of unique study fields records (dictionaries) that match
+            the given program name and degree.
+    """
+    with open(sourcefile, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    added: set[tuple[str, int, str]] = set()
+    records: list = []
+
+    for study_field in data:
+        name = study_field.get("nazwa", None)
+        deg = study_field.get("stopien", None)
+        major = study_field.get("specjalizacja", None)
+        if name is None or deg is None:
+            continue
+
+        if name == study_field_name and deg == degree:
+            if (name, deg, major) not in added:
+                records.append(study_field)
+                added.add((name, deg, major))
+
+    return records
+
+
 if __name__ == "__main__":
     path = "../../data_collector/final-programy.json"
 
     combs = _get_study_field_major_degree_from_file(path, with_major=False)
     for el in combs:
         print(el)
+
+    kierunki = _get_unique_study_fields(path, "informatyka.", 1)
+    print(len(kierunki))
