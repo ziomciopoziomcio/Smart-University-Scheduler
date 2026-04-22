0313f65d8741e6a3bbce8fdbedda33ce6862710f
unknown <kbanaszewskaa@gmail.com>
2026-04-22 14:15:39 +0200

    Fixed copilot11

--- frontend/src/pages/Plans/GeneralPlans/StudyPlanFacultyPage.tsx
+++ frontend/src/pages/Plans/GeneralPlans/StudyPlanFacultyPage.tsx
@@ -46,7 +46,7 @@ export default function StudyPlanFacultyPage() {
         } finally {
             setLoading(false);
         }
-    }, []);
+    }, [intl]);
 
     useEffect(() => {
         loadFaculties();
