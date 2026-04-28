b6a9f1777dcfa92cbdbe6bcad38c431a275aee4c
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-04-28 02:31:12 +0200

    Added missing field

--- backend/src/common/router_utils.py
+++ backend/src/common/router_utils.py
@@ -138,6 +138,7 @@ def serialize_employee_nested(row: tuple[Any, Any, Any, Any]) -> dict:
         "email": user.email,
         "degree": user.degree,
         "created_at": user.created_at,
+        "two_factor_enabled": user.two_factor_enabled,
     }
 
     unit_obj = None
