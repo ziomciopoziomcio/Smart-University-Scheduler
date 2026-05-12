612280de0812a05242ff5b8077c56ad0eedc4a67
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-05-12 15:51:02 +0200

    Added is_active to group's model

--- backend/src/academics/models.py
+++ backend/src/academics/models.py
@@ -79,6 +79,7 @@ class Groups(Base):
         Integer, ForeignKey("elective_block.id")
     )  # Major and elective_block can't be set at the same time
     semester: Mapped[int] = mapped_column(Integer, default=1, server_default="1")
+    is_active: Mapped[bool] = mapped_column(default=True, server_default="true")
 
     __table_args__ = (
         CheckConstraint(
