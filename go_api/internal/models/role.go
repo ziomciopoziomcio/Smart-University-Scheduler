bdf60ba43b86f2197b4f2d33fb15d2029566f1f8
Wojciech Korbel <157359659+WojciechKorbel@users.noreply.github.com>
2026-06-02 21:09:54 +0200

    Refactor
    Co-Authored-By: Nina Zugaj <154976830+ninsson@users.noreply.github.com>

--- go_api/internal/models/role.go
+++ go_api/internal/models/role.go
@@ -1,18 +1,5 @@
 package models
 
-type Permission struct {
-	ID          int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	Code        string `json:"code" gorm:"size:100;unique;index"`
-	Name        string `json:"name" gorm:"size:100;unique"`
-	Description string `json:"description" gorm:"size:200"`
-	Group       string `json:"group" gorm:"size:50"`
-
-	Roles []Role `json:"roles,omitempty" gorm:"many2many:role_permissions;foreignKey:ID;joinForeignKey:PermissionID;References:ID;joinReferences:RoleID"`
-}
-
-func (Permission) TableName() string {
-	return "permissions"
-}
 
 type Role struct {
 	ID       int    `json:"id" gorm:"primaryKey;autoIncrement"`
